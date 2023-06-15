f0eaf5dd4692a8b482b54a88d29961d77ea73e72
Shahzad Durrani <Shahzad@Asifas-MacBook-Pro.local>
2023-06-15 19:04:15 +0500

    Add snippet page added & home page partially done

--- src/Components/Add-snippet.jsx
+++ src/Components/Add-snippet.jsx
@@ -1,6 +1,73 @@
 const AddSnippet = () => {
+
+function handleAdd (){
+    const selection = document.getElementById("snippet-language");
+    let selection_value  = selection.value;
+   const div_snippet = document.getElementById('container-snippet');
+   let snippet    =   "";
+   snippet    =  snippet.concat("<div class='textarea-snippet'><small class='snippetTag'>",selection_value,"</small><textarea class='form-control' snippet='", selection_value, "' placeholder='Add snippet here' ></textarea></div>");
+   selection_value !== '' ? div_snippet.insertAdjacentHTML("beforeend",snippet) : document.getElementById("snippet-language").focus();
+   selection_value = '';
+}
+
     return(
-        <h1>Add</h1>
+        <>
+<div className="container mt-5 mb-5">
+  <div className="row d-flex justify-content-center">
+  <form>
+  
+<div className="row mb-3">
+    <label htmlFor="inputEmail3" className="col-sm-2 col-form-label">Language</label>
+    <div className="col-sm-10 padding-none">
+    <select className="form-select form-select-lg mb-3" aria-label=".form-select-lg example">
+    <option  defaultValue="">Select language</option>
+  <option value="html">Html</option>
+  <option value="scss">Scss</option>
+  <option value="js">JavaScript</option>
+</select>
+    </div>
+  </div>
+  <div className="row mb-3">
+    <label htmlFor="input-title" className="col-sm-2 col-form-label">Title</label>
+    <div className="col-sm-10 padding-none">
+      <input type="title" className="form-control" id="input-title" />
+    </div>
+  </div>
+  <div className="row mb-3">
+    <label htmlFor="input-desc" className="col-sm-2 col-form-label">Description</label>
+    <div className="col-sm-10 padding-none">
+      <input type="desc" className="form-control" id="input-desc" />
+    </div>
+  </div>
+
+  <div className="row mb-3">
+    <label htmlFor="input-code" className="col-sm-2 col-form-label">Add Snippet</label>
+    <div className="col-sm-10">
+    <div className="row container-snippet" id="container-snippet">  
+    <div className="select-lang col-md-4">
+    <select className="form-select form-select-md mb-3 col-md-5" id="snippet-language" aria-label=".form-select-lg example">
+    
+    <option  defaultValue="null" placeholder="Select Language"> </option>
+        <option value="html">Html</option>
+        <option value="scss">Scss</option>
+        <option value="js">JavaScript</option>
+    </select>
+  </div>
+  <div className="btn-add col-md-2">
+  <button type="button" className="btn btn-primary" onClick={handleAdd}>Add</button>
+  </div>
+</div>
+    </div>
+  </div>
+
+  <button type="submit" className="btn btn-primary float-end btn-post">Post</button>
+</form>
+    </div>
+</div>
+
+
+
+        </>
     )
 }
 
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,7 +1,92 @@
+import { useEffect } from "react";
 
 const Home = () => {
+    const fullHeight = () => {
+        const elements = document.querySelectorAll('.js-fullheight');
+  
+        const setFullHeight = () => {
+          const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+          elements.forEach(element => {
+            element.style.height = windowHeight + 'px';
+          });
+        };
+  
+        setFullHeight();
+  
+        window.addEventListener('resize', setFullHeight);
+      };
+
+      function handleNav() {
+    const sidebar = document.getElementById('sidebar');
+    sidebar.classList.toggle('active');
+
+      console.log('click')
+      }
+
+    useEffect(() => {
+        
+      
+          fullHeight();
+
+
+    },[])
+
     return(
-        <h1>Home</h1>
+        <>
+
+
+<div className="wrapper d-flex align-items-stretch js-fullheight" >
+  <nav id="sidebar" className="active ">
+    <h1><a href="index.html" className="logo">Snippet.</a></h1>
+    <ul className="list-unstyled components mb-5">
+      <li className="active">
+      <h5>Categories</h5>
+    <div className="form-check">
+        <input className="form-check-input" type="checkbox" defaultValue id="flexCheckDefault" />
+        <label className="form-check-label" htmlFor="flexCheckDefault">
+            HTML
+        </label>
+    </div>
+    <div className="form-check">
+        <input className="form-check-input" type="checkbox" defaultValue id="flexCheckDefault" />
+        <label className="form-check-label" htmlFor="flexCheckDefault">
+            SCSS
+        </label>
+    </div>
+    <div className="form-check">
+        <input className="form-check-input" type="checkbox" defaultValue id="flexCheckDefault" />
+        <label className="form-check-label" htmlFor="flexCheckDefault">
+            JAVASCRIPT
+        </label>
+    </div>
+    <div className="form-check">
+        <input className="form-check-input" type="checkbox" defaultValue id="flexCheckDefault" />
+        <label className="form-check-label" htmlFor="flexCheckDefault">
+            PHP
+        </label>
+    </div>
+      </li>
+    </ul>
+  </nav>
+  {/* Page Content  */}
+  <div id="content" className="p-4 p-md-5">
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <div className="container-fluid">
+        <button type="button" onClick={handleNav} id="sidebarCollapse" className="btn btn-primary">
+          <i className="fa fa-bars" />
+          <span className="sr-only">Toggle Menu</span>
+        </button>
+        
+      </div>
+    </nav>
+    <h2 className="mb-4">Sidebar #07</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+  </div>
+</div>
+
+
+        </>
     )
 }
 
